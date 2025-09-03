060cb3194dda6bc57574c838eb5c76d4b60915c5
Charith_K <23071a6628@vnrvjiet.in>
2025-09-03 15:40:44 +0530

    reinstalled the node modules

---
+++ src/firebase.js
@@ -0,0 +1,7 @@
+// Minimal firebase stub to satisfy imports during build.
+// Exports null placeholders so existing imports don't fail.
+
+export const auth = null;
+export const db = null;
+
+export default { auth, db };
